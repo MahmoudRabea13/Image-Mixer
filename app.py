30aeffff0208d900b4e9a66c3548c16fdb3b3204
AhmedHassan178 <87389435+AhmedHassan178@users.noreply.github.com>
2023-01-02 14:46:41 +0200

    code repetation

--- app.py
+++ app.py
@@ -11,23 +11,6 @@ import matplotlib.pyplot as plt
 import os
 import sys
 app = Flask(__name__)
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     return render_template('index.html')
-# @app.route('/get', methods=['GET', 'POST'])
-# def get(jsdata):
-#     print(jsdata)
-#     return 'success'
-# @app.route('/post', methods=['GET', 'POST'])
-# def post():
-#     print('hello')
-#     print(request.form['data'])
-#     return 'success'
-# if __name__ == '__main__':
-#     app.run()
-
-########################################### Misara #######################################
 r = 0
 @app.route("/" ,methods=['POST','GET'])
 def image():
@@ -39,50 +22,12 @@ def image():
         try:
             r = request.get_json()
             list_of_data = r['data']
-            if len(list_of_data)>6 and list_of_data[1] == None:
-                for i in range(4):
-                    list_of_data[i] = 0
-            for i in range(len(list_of_data)):
-                list_of_data[i] = int(list_of_data[i]*800/369)
-            list_of_data.insert(0,Functions.cut_or_keep)
-            if Functions.var[0] == 0:
-                if list_of_data[3]==0:
-                    list_of_data.insert(5,Functions.cut_or_keep)
-                    list_of_data.append(0)
-                    Functions.cutting_mat = [list_of_data[5:]]
-                    print(Functions.cutting_mat)
-                else:
-                    if len(list_of_data)>6:
-                        list_of_data.insert(5,1)
-                        list_of_data.insert(6,Functions.cut_or_keep)
-                        list_of_data.append(0)
-                        Functions.cutting_mat = [list_of_data[:6],list_of_data[6:]]
-                        print(Functions.cutting_mat)
-                    if len(list_of_data) <= 6:
-                        list_of_data.append(1)
-                        Functions.cutting_mat = [list_of_data[:6]]
-                        print(Functions.cutting_mat)    
-            else:
-                if list_of_data[4]==0:
-                    list_of_data.insert(5,Functions.cut_or_keep)
-                    list_of_data.append(0)
-                    Functions.cutting_mat = [list_of_data[5:]]
-                    print(Functions.cutting_mat)
-                else:
-                    if len(list_of_data)>6:
-                        list_of_data.insert(5,0)
-                        list_of_data.insert(6,Functions.cut_or_keep)
-                        list_of_data.append(1)
-                        Functions.cutting_mat = [list_of_data[:6],list_of_data[6:]]
-                        print(Functions.cutting_mat)
-                    if len(list_of_data) <= 6:
-                        list_of_data.append(0)
-                        Functions.cutting_mat = [list_of_data[:6]]
-                        print(Functions.cutting_mat) 
+            new_list = Functions.json_to_list(list_of_data)
+            new_list = Functions.dimension(new_list)
+            new_list = Functions.append_state(Functions.cut_or_keep,Functions.var[0],new_list)
+            Functions.cutting_mat = new_list
         except:
-            Functions.cutting_mat = [[0,0,0,800,800,1]]
-            print('hello')
-            pass
+            Functions.cutting_mat =[[],[]]
         if select_cut != None:
             if select_cut.filename == '1':
                 Functions.cut_or_keep = 0
@@ -99,35 +44,22 @@ def image():
             Functions.fourier_1 = Functions.fourier('./image1.jpg')
             Functions.fouriers_list[0] = Functions.magphase(Functions.fourier_1, 'mag')
             Functions.fouriers_list[1] = Functions.magphase(Functions.fourier_1, 'phase')
-            Functions.flag_1 = True
         if img2 != None:
             imgs_save2 = img2.filename + '.jpg'
             img2.save(imgs_save2)
             Functions.fourier_2 = Functions.fourier('./image2.jpg')
             Functions.fouriers_list[2] = Functions.magphase(Functions.fourier_2, 'mag')
             Functions.fouriers_list[3] = Functions.magphase(Functions.fourier_2, 'phase')
-            Functions.flag_2 = True
         Functions.temp_fourier_1 = Functions.fouriers_list[Functions.var[0]]
         Functions.temp_fourier_2 = Functions.fouriers_list[Functions.var[1]]
         for i in range(len(Functions.cutting_mat)):
-            if Functions.cutting_mat[i][5] == 0:
-                if Functions.cutting_mat[i][0] == 0:
-                    Functions.temp_fourier_2 = Functions.cut(Functions.temp_fourier_2,Functions.cutting_mat[i][1],Functions.cutting_mat[i][2],Functions.cutting_mat[i][3],Functions.cutting_mat[i][4],'keep')
-                if Functions.cutting_mat[i][0] == 1:
-                    Functions.temp_fourier_2 = Functions.cut(Functions.temp_fourier_2,Functions.cutting_mat[i][1],Functions.cutting_mat[i][2],Functions.cutting_mat[i][3],Functions.cutting_mat[i][4],'cut')
-            if Functions.cutting_mat[i][5] == 1:
-                if Functions.cutting_mat[i][0] == 0:
-                    Functions.temp_fourier_1 = Functions.cut(Functions.fouriers_list[Functions.var[0]],Functions.cutting_mat[i][1],Functions.cutting_mat[i][2],Functions.cutting_mat[i][3],Functions.cutting_mat[i][4],'keep')
-                if Functions.cutting_mat[i][0] == 1:
-                    Functions.temp_fourier_1 = Functions.cut(Functions.fouriers_list[Functions.var[0]],Functions.cutting_mat[i][1],Functions.cutting_mat[i][2],Functions.cutting_mat[i][3],Functions.cutting_mat[i][4],'cut')
-        # Functions.fouriers_list[Functions.var[0]] =  Functions.temp_fourier_2   
-        if Functions.var[0] == 0:
-            Functions.visulaize(np.log(Functions.fouriers_list[Functions.var[0]]),1)
-            Functions.visulaize(Functions.fouriers_list[Functions.var[1]],2)
-        elif Functions.var[0] == 2:
-            Functions.visulaize(np.log(Functions.fouriers_list[Functions.var[0]]),2)
-            Functions.visulaize(Functions.fouriers_list[Functions.var[1]],1)
-        re_image = Functions.re_fourier(Functions.temp_fourier_1,Functions.temp_fourier_2)
+            if len(Functions.cutting_mat[i]) > 2:
+                if Functions.cutting_mat[i][5]==0:
+                    Functions.temp_fourier_2 =  Functions.cut(Functions.fouriers_list[Functions.var[1]],Functions.cutting_mat[i][:5])
+                else:
+                    Functions.temp_fourier_1 = Functions.cut(Functions.fouriers_list[Functions.var[0]],Functions.cutting_mat[i][:5])
+        Functions.show_image(Functions.var[0])
+        re_image = Functions.mix_fourier(Functions.temp_fourier_1,Functions.temp_fourier_2)
         Functions.visulaize(np.abs(re_image),3)
         return render_template('main.html')
     else:
--- functions.py
+++ functions.py
@@ -30,21 +30,49 @@ class Functions():
             return np.abs(fourier)
         elif select == 'phase':
             return np.angle(fourier)
-    def cut(fourier, x, y,width, height, select):
-        if select == 'cut':
+    def cut(fourier, list_dimensions):
+        if list_dimensions[0] == 1:
             cut_mat = np.ones((fourier.shape[0], fourier.shape[1]))
-            cut_mat[y:y+height,x:x+width] = 0
-        elif select == 'keep':
+            cut_mat[list_dimensions[2]:list_dimensions[2]+list_dimensions[4],list_dimensions[1]:list_dimensions[1]+list_dimensions[3]] = 0
+        elif list_dimensions[0] == 0:
             cut_mat = np.zeros((fourier.shape[0], fourier.shape[1]))
-            cut_mat[y:y+height,x:x+width] = 1
+            cut_mat[list_dimensions[2]:list_dimensions[2]+list_dimensions[4],list_dimensions[1]:list_dimensions[1]+list_dimensions[3]] = 1
         return fourier*cut_mat
     def visulaize(fourier,number):
         plt.figure()
         plt.imshow(fourier, cmap='gray')
         plt.axis('off')
         plt.savefig(f'static/{number}.jpg', bbox_inches='tight', pad_inches=0)
-    def re_fourier(magnitude,phase):
+    def mix_fourier(magnitude,phase):
         re_fourier = magnitude*np.exp(1j*phase)
+        re_fourier= np.fft.ifftshift(re_fourier)
         re_fourier = np.fft.ifft2(re_fourier)
         return re_fourier
-    
+    def dimension(list_of_dim):
+        for i in range(len(list_of_dim)):
+            for j in range(len(list_of_dim[i])):
+                list_of_dim[i][j] = int(list_of_dim[i][j]*800/369)
+        return list_of_dim
+    def json_to_list(list_of_data):
+        if len(list_of_data) > 4:
+            if list_of_data[0] == None:
+                cutting_list = [[],list_of_data[4:]]
+            else:
+                cutting_list = [list_of_data[:4],list_of_data[4:]]
+        else:
+            cutting_list = [list_of_data[:4],[]]
+        return cutting_list
+    def append_state(selection,item,list_of_data):
+        phase_falg = True
+        for i in range(len(list_of_data)):
+            list_of_data[i].insert(0,selection)
+            if item == 0 and phase_falg:
+                list_of_data[i].append(1)
+                phase_falg = False
+            else:
+                list_of_data[i].append(0)
+        return list_of_data
+    def show_image(order):
+        order_list = [[1,2],[],[2,1]]
+        Functions.visulaize(np.log(Functions.fouriers_list[Functions.var[0]]),order_list[order][0])
+        Functions.visulaize(Functions.fouriers_list[Functions.var[1]],order_list[order][1])
